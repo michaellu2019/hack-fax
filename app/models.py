c3216dca9219e43ac16f6638dc846a5362dae774
Michael Lu <mlu0708@mit.edu>
2021-04-15 05:12:24 -0400

    Changed model config

--- app/models.py
+++ app/models.py
@@ -3,8 +3,8 @@ from sqlalchemy.dialects import mysql
 
 class Fact(db.Model):
 	pk_id = db.Column(db.Integer, primary_key = True)
-	name = db.Column(mysql.VARCHAR(100), nullable = False)
-	fact = db.Column(mysql.VARCHAR(500), nullable = False)
+	name = db.Column(mysql.VARCHAR(1000), nullable = False)
+	fact = db.Column(mysql.VARCHAR(1000), nullable = False)
 
 	def __repr__(self):
 		return f'Fact(\'{self.name}\', \'{self.fact}\')'
